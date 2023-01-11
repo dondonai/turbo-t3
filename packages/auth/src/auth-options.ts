bbc0d5b50607311edf0a1eaf8c399e30a172c6ae
dondonai <adones.kjc@gmail.com>
2023-01-11 16:23:43 +0800

    updated env

--- packages/auth/src/auth-options.ts
+++ packages/auth/src/auth-options.ts
@@ -1,5 +1,5 @@
 import { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
+import GoogleProvider from "next-auth/providers/google"
 
 import { prisma } from "@acme/db";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
@@ -8,11 +8,11 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   adapter: PrismaAdapter(prisma),
   providers: [
-    DiscordProvider({
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+    GoogleProvider({
+
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
-    // ...add more providers here
   ],
   callbacks: {
     session({ session, user }) {
